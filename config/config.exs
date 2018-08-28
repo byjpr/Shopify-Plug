b6c5d9360ea39a05ebc91127d03cb55062fc7a23
Jordan Parker <letsgetjordan@gmail.com>
2018-08-29 00:31:32 +0100

    Removes config

--- config/config.exs
+++
@@ -1,7 +0,0 @@
-# This file is responsible for configuring your application
-# and its dependencies with the aid of the Mix.Config module.
-use Mix.Config
-
-config :shopify_plug,
-  shopify_client_secret: nil,
-  rule: :block
