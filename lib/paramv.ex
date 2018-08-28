2d86a781a0f4384b5e8f4aaa205ce81e14968999
Jordan Parker <letsgetjordan@gmail.com>
2018-08-29 00:31:25 +0100

    Paramv

---
+++ lib/paramv.ex
@@ -0,0 +1,30 @@
+defmodule ShopifyPlug.Paramv do
+  @behaviour Plug
+
+  def init(), do: []
+  def init(default), do: default
+
+  @doc """
+  Check that all the parameters we need are set in the connection.
+  """
+  def call(
+        %Plug.Conn{
+          params: %{
+            "shop" => _shop,
+            "signature" => _signature,
+            "timestamp" => _timestamp,
+            "path_prefix" => _path_prefix
+          }
+        } = conn,
+        _default
+      ) do
+    conn
+  end
+
+  @doc """
+  If the parameters are not set we'll render an error and halt the pipeline
+  """
+  def call(conn, _default) do
+    ShopifyPlug.Errors.failed_connection(conn, :Paramv)
+  end
+end
