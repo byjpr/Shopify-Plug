b19fef8ca75c323cd24f9e79a525d50a132db660
Jordan Parker <letsgetjordan@gmail.com>
2018-08-28 12:26:32 +0100

    Create config

---
+++ config/config.exs
@@ -0,0 +1,7 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+use Mix.Config
+
+config :shopify_plug,
+  shopify_client_secret: nil,
+  rule: :block
