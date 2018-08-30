13edc86d65dc4ca445f9af3a08f581914f519cf1
Jordan Parker <letsgetjordan@gmail.com>
2018-08-30 21:30:26 +0100

    Paramv test

---
+++ test/paramv_test.exs
@@ -0,0 +1,35 @@
+defmodule ShopifyPlug.ParamvTest do
+  use ExUnit.Case
+  import PlugHelper
+
+  @query "extra=1&" <>
+           "extra=2&" <>
+           "shop=shop-name.myshopify.com&" <>
+           "path_prefix=%2Fapps%2Fawesome_reviews&" <>
+           "timestamp=1317327555&" <>
+           "signature=a9718877bea71c2484f91608a7eaea1532bdf71f5c56825065fa4ccabe549ef3"
+
+  @missing_timestamp "extra=1&" <>
+          "extra=2&" <>
+          "shop=shop-name.myshopify.com&" <>
+          "path_prefix=%2Fapps%2Fawesome_reviews&" <>
+          "signature=a9718877bea71c2484f91608a7eaea1532bdf71f5c56825065fa4ccabe549ef3"
+
+  describe "plug" do
+    test "Paramv" do
+      %{url: "/paramv", query: @query}
+      |> make_request()
+      |> fetch_all()
+      |> ShopifyPlug.Paramv.call([])
+      |> assert_authorized()
+    end
+
+    test "missing_timestamp" do
+      %{url: "/paramv", query: @missing_timestamp}
+      |> make_request()
+      |> fetch_all()
+      |> ShopifyPlug.Paramv.call([])
+      |> assert_unauthorized()
+    end
+  end
+end
