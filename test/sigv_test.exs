c360b86969c9089bcd154282f18390acb6b7705f
Jordan Parker <letsgetjordan@gmail.com>
2018-08-30 21:30:08 +0100

    Test sigv

--- test/sigv_test.exs
+++ test/sigv_test.exs
@@ -1,44 +1,20 @@
 defmodule ShopifyPlug.SigvTest do
   use ExUnit.Case
-  use Phoenix.ConnTest
+  import PlugHelper
 
-  @secret "hush"
-  @query "extra=1&" <>
-           "extra=2&" <>
-           "shop=shop-name.myshopify.com&" <>
-           "path_prefix=%2Fapps%2Fawesome_reviews" <>
-           "&timestamp=1317327555&" <>
-           "signature=a9718877bea71c2484f91608a7eaea1532bdf71f5c56825065fa4ccabe549ef3"
-
-  defp assert_unauthorized(conn) do
-    assert conn.status == 400
-    assert conn.halted
-  end
-
-  defp assert_authorized(conn) do
-    assert conn.status != 403
-    refute conn.halted
-  end
-
-  # make_request(%{url: "/", query: @query, secret: @secret})
-  defp make_request(%{url: url, query: query, secret: secret}) do
-    endpoint = url <> "?" <> query
-
-    build_conn(:post, endpoint, "")
-    |> ShopifyPlug.Sigv.call(secret: secret)
-  end
-
-  @tag :urlencoded_valid_request
-  test "urlencoded request with valid hmac" do
-    %{url: "/sigv", query: @query, secret: @secret}
+  test "Correct signature" do
+    %{url: "/sigv", query: "extra=1&extra=2&shop=shop-name.myshopify.com&path_prefix=%2Fapps%2Fawesome_reviews&timestamp=1317327555&signature=a9718877bea71c2484f91608a7eaea1532bdf71f5c56825065fa4ccabe549ef3"}
     |> make_request()
+    |> fetch_all()
+    |> ShopifyPlug.Sigv.call([signature: "hush"])
     |> assert_authorized()
   end
 
-  @tag :urlencoded_valid_request
-  test "urlencoded request with invalid hmac" do
-    %{url: "/sigv", query: @query, secret: "1234"}
+  test "Incorrect signature" do
+    %{url: "/sigv", query: "extra=1&extra=2&shop=shop-name.myshopify.com&path_prefix=%2Fapps%2Fawesome_reviews&timestamp=1317327555&signature=QWxsIGNvbW11bmljYXRpb25zIHdpdGggb3VyIHNlcnZlcnMgYXJlIG1hZGUgdGhyb3"}
     |> make_request()
+    |> fetch_all()
+    |> ShopifyPlug.Sigv.call([signature: "hush"])
     |> assert_unauthorized()
   end
 end
