1e292b67b4d6b090b99941ed342e05f8b5340aee
Jordan Parker <letsgetjordan@gmail.com>
2018-08-31 17:15:56 +0100

    Improve coverage

--- test/stampscale_test.exs
+++ test/stampscale_test.exs
@@ -26,11 +26,21 @@ defmodule ShopifyPlug.StampscaleTest do
     |> assert_unauthorized()
   end
 
-  test "No init options" do
+  test "No call/init options" do
     %{url: "/stampscale", query: "extra=1&extra=2&shop=shop-name.myshopify.com&path_prefix=%2Fapps%2Fawesome_reviews&timestamp=1317327555&signature=a9718877bea71c2484f91608a7eaea1532bdf71f5c56825065fa4ccabe549ef3"}
     |> make_request()
     |> fetch_all()
     |> ShopifyPlug.StampScale.call()
     |> assert_unauthorized()
+  end
+
+  test "No init options" do
+    init = ShopifyPlug.StampScale.init()
+    assert init == []
+  end
+
+  test "init options" do
+    init = ShopifyPlug.StampScale.init([sample: "options"])
+    assert init == [sample: "options"]
   end
 end
