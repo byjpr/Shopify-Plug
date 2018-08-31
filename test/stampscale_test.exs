2b4cd2e0b7aa807492270d914a041f58538328d0
Jordan Parker <letsgetjordan@gmail.com>
2018-08-31 17:09:50 +0100

    improve coverage

--- test/stampscale_test.exs
+++ test/stampscale_test.exs
@@ -9,4 +9,28 @@ defmodule ShopifyPlug.StampscaleTest do
     |> ShopifyPlug.StampScale.call([])
     |> assert_unauthorized()
   end
+
+  test "No timestamp param" do
+    %{url: "/stampscale", query: "extra=1&extra=2&shop=shop-name.myshopify.com&path_prefix=%2Fapps%2Fawesome_reviews"}
+    |> make_request()
+    |> fetch_all()
+    |> ShopifyPlug.StampScale.call([])
+    |> assert_unauthorized()
+  end
+
+  test "No params" do
+    %{url: "/stampscale", query: ""}
+    |> make_request()
+    |> fetch_all()
+    |> ShopifyPlug.StampScale.call([])
+    |> assert_unauthorized()
+  end
+
+  test "No init options" do
+    %{url: "/stampscale", query: "extra=1&extra=2&shop=shop-name.myshopify.com&path_prefix=%2Fapps%2Fawesome_reviews&timestamp=1317327555&signature=a9718877bea71c2484f91608a7eaea1532bdf71f5c56825065fa4ccabe549ef3"}
+    |> make_request()
+    |> fetch_all()
+    |> ShopifyPlug.StampScale.call()
+    |> assert_unauthorized()
+  end
 end
